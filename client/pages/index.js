817161bff2d63f0ad1d33128400029218bf524cd
Federico <federicolucaspontoriero@gmail.com>
2022-08-20 17:24:56 -0300

    override the app component and basic navbar

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="col">
-        <h1>Home Page</h1>
+        <h1 className="display-1 text-center py-5">Home Page</h1>
       </div>
     </div>
   );
