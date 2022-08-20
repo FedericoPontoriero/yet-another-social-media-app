aa640b8df739bd9e22314d46fe0281a99985c39b
Federico <federicolucaspontoriero@gmail.com>
2022-08-19 21:33:03 -0300

    added dependencies & first pages

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="col">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
