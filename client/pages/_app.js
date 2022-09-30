99657e34dfe43731c7f858cba7bf034bf10cbfcc
Federico <federicolucaspontoriero@gmail.com>
2022-09-30 15:53:44 -0300

    added toastify and antd

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,19 @@
+import { ToastContainer } from 'react-toastify'
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-toastify/dist/ReactToastify.css'
+import 'antd/dist/antd.css'
 
 import Nav from "../components/Nav";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/css/styles.css" />
+      </Head>
       <Nav />
+      <ToastContainer position='top-center' />
       <Component {...pageProps} />
     </>
   );
