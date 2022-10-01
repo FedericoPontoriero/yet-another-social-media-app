8ad5d28ad26eef4ebe6ca13c917bef54213ec5a8
Federico <federicolucaspontoriero@gmail.com>
2022-10-01 18:58:49 -0300

    endpoint for user verification

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,56 +1,64 @@
 import User from "../models/user";
-import { hashPassword, comparePassword } from '../helpers/auth'
-import jwt from 'jsonwebtoken';
-
+import { hashPassword, comparePassword } from "../helpers/auth";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { name, email, password, secret } = req.body;
-  if (!name)
-    return res.status(400).send('Name is required');
-  if (!email)
-    return res.status(400).send('Email is required');
+  if (!name) return res.status(400).send("Name is required");
+  if (!email) return res.status(400).send("Email is required");
   if (!password || password.length < 6)
-    return res.status(400).send('Password is required & should be 6 characters minimum');
-  if (!secret)
-    return res.status(400).send('Answer is required');
-  const exists = await User.findOne({ email })
-  if (exists) return res.status(400).send('Email is taken')
+    return res
+      .status(400)
+      .send("Password is required & should be 6 characters minimum");
+  if (!secret) return res.status(400).send("Answer is required");
+  const exists = await User.findOne({ email });
+  if (exists) return res.status(400).send("Email is taken");
 
-  const hashedPassword = await hashPassword(password)
+  const hashedPassword = await hashPassword(password);
 
-  const user = new User({ name, email, password: hashedPassword, secret })
+  const user = new User({ name, email, password: hashedPassword, secret });
   try {
-    await user.save()
+    await user.save();
     return res.json({
-      ok: true
-    })
+      ok: true,
+    });
   } catch (e) {
-    console.log('register failed => ', err);
-    return res.status(400).send('Error, try again')
+    console.log("register failed => ", err);
+    return res.status(400).send("Error, try again");
   }
 };
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = await User.findOne({ email })
-    if (!user) return res.status(400).send('No user found')
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send("No user found");
 
-    const match = await comparePassword(password, user.password)
-    if (!match) return res.status(400).send('Wrong password')
+    const match = await comparePassword(password, user.password);
+    if (!match) return res.status(400).send("Wrong password");
 
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
     user.password = undefined;
     user.secret = undefined;
     res.json({
       token,
-      user
+      user,
     });
-
   } catch (err) {
     console.log(err);
-    return res.status(400).send('Error, try again');
+    return res.status(400).send("Error, try again");
   }
+};
 
-}
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const { register, login } = require("../controllers/auth");
+const { register, login, currentUser } = require("../controllers/auth");
+const { requireSignIn } = require("../middlewares/auth");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/current-user", requireSignIn, currentUser);
 
 module.exports = router;
