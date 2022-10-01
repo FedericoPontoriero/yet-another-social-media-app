52737b1c8e35fb5e801b070708e2b4e180c1cf3a
Federico <federicolucaspontoriero@gmail.com>
2022-10-01 16:46:59 -0300

    created context  for state management

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,20 +2,22 @@ import { ToastContainer } from 'react-toastify'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'react-toastify/dist/ReactToastify.css'
 import 'antd/dist/antd.css'
+import { UserProvider } from '../context';
+
 
 import Nav from "../components/Nav";
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserProvider>
       <Head>
         <link rel="stylesheet" href="/css/styles.css" />
       </Head>
       <Nav />
       <ToastContainer position='top-center' />
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   );
 }
 
