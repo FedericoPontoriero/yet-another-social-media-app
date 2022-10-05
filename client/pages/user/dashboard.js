dc2c1299e054b683ddd9532cea2e5847bd9b6176
Federico <federicolucaspontoriero@gmail.com>
2022-10-05 11:25:08 -0300

    basic post form

--- client/pages/user/dashboard.js
+++ client/pages/user/dashboard.js
@@ -2,17 +2,24 @@ import { useContext } from "react";
 import { UserContext } from "../../context";
 
 import UserRoute from "../../components/routes/UserRoute";
+import CreatePostForm from "../../components/forms/CreatePostForm";
 
 const Home = () => {
   const [state, setState] = useContext(UserContext);
 
   return (
     <UserRoute>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="display-1 text-center">Dashboard page</h1>
+      <div className="container-fluid">
+        <div className="row py-5 bg-default-image text-light">
+          <div className="col text-center">
+            <h1>Newsfeed</h1>
           </div>
+        </div>
+        <div className="row py-3">
+          <div className="col-md-8">
+            <CreatePostForm />
+          </div>
+          <div className="col-md-4">Sidebar</div>
         </div>
       </div>
     </UserRoute>
