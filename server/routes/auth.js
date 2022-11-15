557babd2fec89ebd085ced547e499e31fc134e0a
Federico <federicolucaspontoriero@gmail.com>
2022-11-15 15:56:53 -0300

    following in the server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,6 +9,8 @@ const {
   profileUpdate,
   findPeople,
   forgotPassword,
+  addFollower,
+  userFollow,
 } = require("../controllers/auth");
 const { requireSignIn } = require("../middlewares/auth");
 
@@ -18,5 +20,5 @@ router.get("/current-user", requireSignIn, currentUser);
 router.post("/forgot-password", forgotPassword);
 router.put("/profile-update", requireSignIn, profileUpdate);
 router.get("/find-people", requireSignIn, findPeople);
-
+router.put("/user-follow", requireSignIn, addFollower, userFollow);
 module.exports = router;
