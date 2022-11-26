83d8bca5aa370c38f50c39f8c4b235a93819ddb6
Federico <federicolucaspontoriero@gmail.com>
2022-11-26 04:16:50 -0300

    follow for search users

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,11 +1,12 @@
 import { useState, useContext } from "react";
 import axios from "axios";
+import toast from "react-toastify";
 
 import { UserContext } from "../context";
 import People from "../components/cards/People";
 
 const Search = () => {
-  const [state] = useContext(UserContext);
+  const [state, setState] = useContext(UserContext);
 
   const [query, setQuery] = useState("");
   const [result, setResult] = useState([]);
@@ -16,6 +17,39 @@ const Search = () => {
     try {
       const { data } = await axios.get(`/search-user/${query}`);
       setResult(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleFollow = async (user) => {
+    try {
+      const { data } = await axios.put("/user-follow", { _id: user._id });
+      let auth = JSON.parse(localStorage.getItem("auth"));
+      // update local storage, update user, keep token
+      auth.user = data;
+      localStorage.setItem("auth", JSON.stringify(auth));
+      // update context
+      setState({ ...state, user: data });
+      // update people state
+      let filtered = result.filter((p) => p._id !== user._id);
+      setResult(filtered);
+      toast.success(`Following ${user.name}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnfollow = async (user) => {
+    try {
+      const { data } = await axios.put("/user-unfollow", { _id: user._id });
+      let auth = JSON.parse(localStorage.getItem("auth"));
+      auth.user = data;
+      localStorage.setItem("auth", JSON.stringify(auth));
+      setState({ ...state, user: data });
+      let filtered = result.filter((p) => p._id !== user._id);
+      setResult(filtered);
+      toast.error(`Unfollowed ${user.name}`);
     } catch (err) {
       console.log(err);
     }
@@ -42,7 +76,15 @@ const Search = () => {
           </button>
         </div>
       </form>
-      {result && result.map((r) => <People key={r._id} people={result} />)}
+      {result &&
+        result.map((r) => (
+          <People
+            handleFollow={handleFollow}
+            handleUnfollow={handleUnfollow}
+            key={r._id}
+            people={result}
+          />
+        ))}
     </>
   );
 };
