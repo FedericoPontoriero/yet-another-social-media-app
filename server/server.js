3db6423f69f20f1275c6046d46c3da78f765d775
Federico <federicolucaspontoriero@gmail.com>
2022-12-04 16:11:57 -0300

    socket.io server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 const { readdirSync } = require("fs");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-type"],
+  },
+});
 
 const app = express();
 
@@ -15,8 +23,19 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB connection error", err));
 
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
+io.on("connect", (socket) => {
+  console.log("SOCKET.IO =>", socket.id);
+});
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+http.listen(port, () => console.log(`Server running on port: ${port}`));
